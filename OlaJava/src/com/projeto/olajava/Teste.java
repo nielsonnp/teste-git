b49e414462e2c2ce64d286d2d1e7fe5dcc3b1fb3
Ranyelson Dantas <nielsonnp@gmail.com>
2021-10-18 17:50:04 -0300

    Initial commit

---
+++ OlaJava/src/com/projeto/olajava/Teste.java
@@ -0,0 +1,16 @@
+package com.projeto.olajava;
+
+public class Teste {
+
+	public static void main(String[] args) {
+		int num1 = 2;
+		int num2 = 6;
+		int resultado = 0;
+		
+		resultado = num1 * num2;
+		
+		System.out.println( resultado );
+		
+	}
+
+}

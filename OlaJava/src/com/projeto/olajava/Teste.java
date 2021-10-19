eb2bb73cb06e943cd32f38172847f35c26680f16
PC_3 <PC_3@SETOR_CAMPO>
2021-10-19 08:07:14 -0300

    Alterando operação

--- OlaJava/src/com/projeto/olajava/Teste.java
+++ OlaJava/src/com/projeto/olajava/Teste.java
@@ -3,11 +3,11 @@ package com.projeto.olajava;
 public class Teste {
 
 	public static void main(String[] args) {
-		int num1 = 2;
+		int num1 = 12;
 		int num2 = 6;
 		int resultado = 0;
 		
-		resultado = num1 + num2;
+		resultado = num1 - num2;
 		
 		System.out.println(resultado);
 		

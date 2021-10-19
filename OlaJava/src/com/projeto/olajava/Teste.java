6f10878cd6904155aa5faa67b804d58b2b44b8af
PC_3 <PC_3@SETOR_CAMPO>
2021-10-19 08:14:16 -0300

    If-Else

--- OlaJava/src/com/projeto/olajava/Teste.java
+++ OlaJava/src/com/projeto/olajava/Teste.java
@@ -3,11 +3,15 @@ package com.projeto.olajava;
 public class Teste {
 
 	public static void main(String[] args) {
-		int num1 = 12;
+		int num1 = 10;
 		int num2 = 6;
 		int resultado = 0;
 		
 		resultado = num1 - num2;
+		if(resultado >= 0) {
+			System.out.println("Positivo");
+		}else
+			System.out.println("Negativo");
 		
 		System.out.println(resultado);
 		
